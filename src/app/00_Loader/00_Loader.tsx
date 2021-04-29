2f4a8b075cbafdbe31677b2e0d0f87a023c8a132
John allou <allou.john@hotmail.fr>
2021-04-29 16:02:19 +0200

    storage, et QRCode

---
+++ src/app/00_Loader/00_Loader.tsx
@@ -0,0 +1,13 @@
+//Ajouter Ecran de chargement
+
+
+import {View} from "react-native";
+import styles from "../../Style/Style";
+import React from "react";
+
+function Loader(){
+  return (
+    <View style={styles.center2}>
+      </View>
+  );
+}
